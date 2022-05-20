81083adc4bfb552adb12df15cabc19f0a2477423
Piotr Szmurlo <piotr.szmurlo7@gmail.com>
2022-05-20 19:13:34 +0200

    spring initializr

---
+++ src/main/java/me/piotrszmurlo/pepescookbook/PepesCookbookApplication.java
@@ -0,0 +1,13 @@
+package me.piotrszmurlo.pepescookbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PepesCookbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PepesCookbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/piotrszmurlo/pepescookbook/PepesCookbookApplicationTests.java
@@ -0,0 +1,13 @@
+package me.piotrszmurlo.pepescookbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PepesCookbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
